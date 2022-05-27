2627cfbc3d28464ca2a7f29229f5c4cdbf11b226
Julie Corbeel <julie.corbeel@gmail.com>
2022-05-27 11:34:36 +0100

    essaye d'anim avec Monsieur Terranova

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,15 @@
-console.info('Hello world');
+import { gsap } from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".patte", {
+    opacity :0, 
+    duration :1 , 
+    stagger: 0.3,
+    scrollTrigger:{ 
+        trigger: ".test--svg",
+        start: "bottom bottom",
+        
+}
+})
