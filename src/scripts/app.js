d496f590d43448093985d02327884a4fcb5a7b10
Julie Corbeel <julie.corbeel@gmail.com>
2022-05-31 15:19:27 +0100

    suppression de toutes les images plus jaout d'images optimisées

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,25 @@
-/*import { gsap } from "gsap";
+import { gsap } from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
+var images = document.querySelectorAll('.img--animation');
+for (let image of images){
+    gsap.from(image,{
+        translateX: 400,
+        duration: 0.5, 
+        ease: "power 4",
+        scrollTrigger:{ 
+            trigger: image,
+            start: "bottom bottom",       
+ }
+ })
+
+}
+
+
+
+/*
 gsap.from(".patte", {
     opacity :0, 
     duration :1 , 
