46ac5f79e99457101b996cf3f5072be890f8799c
Julie Corbeel <julie.corbeel@gmail.com>
2022-06-02 20:01:48 +0100

    essaie d'un hover en gsap réussi mais sûrement pas comme mr colienne voudrait

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import { gsap } from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
@@ -28,8 +30,51 @@ gsap.from (".block--download",{
 
 })
 
+const buttons = document.querySelectorAll('.logo--download')
+for (let button of buttons){
+    button.addEventListener("hover",()=>{
+        gsap.to('.logo--download',{
+            scale: 1.12, 
+            duration: 0.5,
+            
+        })
+        button.addEventListener("mouseenter", () => hover.play());
+        button.addEventListener("mouseleave", () => hover.reverse());
+    })
+}
+
+/*
+gsap.utils.toArray(".logo--download").forEach(img => {
+    let hover = gsap.to(img, {
+        scale: 1.12, 
+        duration: 0.5, 
+        paused: true, 
+        ease: "power1.inOut"});
+        img.addEventListener("mouseenter", () => hover.play());
+        img.addEventListener("mouseleave", () => hover.reverse());
+  });
 
 
+
+/*
+let download = document.querySelector(".logo");
+
+let animation = gsap.to(".logo--download", {
+    paused: true,
+    scale:2,
+});
+download.addEventListener("mouseenter", () => animation.play());
+download.addEventListener("mouseleave", () => animation.reverse());
+*/
+/*
+gsap.to(".logo--download",{
+    scale:2,
+    scrollTrigger:{ 
+        trigger: ".testimony--com3",
+        start: "bottom center",       
+ }
+})
+*/
 /*
 gsap.from(".patte", {
     opacity :0, 
